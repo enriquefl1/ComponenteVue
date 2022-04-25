311397c3d6a84f9711424cb3e2c0b2f6b3952703
Enrique Flores <enrique.flores.lopez@hotmail.com>
2022-04-25 02:55:12 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ComponenteVue
+      v-for="imagen in imagenesData"
+      :key="imagen.nombre"
+      :data="imagen"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ComponenteVue from "./components/ComponenteVue.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ComponenteVue,
+  },
+  data() {
+    return {
+      imagenesData: [
+        {
+          nombre: "Rick Sanchez",
+          src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        },
+        {
+          nombre: "Morty Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        },
+        {
+          nombre: "Summer Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        },
+        {
+          nombre: "Beth Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        },
+        {
+          nombre: "Jerry Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
+body {
+  background-image: url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png");
+  background-size: cover;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/ComponenteVue.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img class="nodo" :src="data.src" alt="" />
+    <h2>{{ data.nombre }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "imagenesData",
+  props: {
+    data() {
+      return {
+        type: Object,
+        default: () => {},
+      };
+    },
+  },
+};
+</script>
+<style scoped>
+.nodo {
+  margin: 0 auto;
+  border-radius: 50%;
+  border: 2px solid #ccc;
+}
+</style>
